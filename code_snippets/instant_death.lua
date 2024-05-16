fd99adf6be714a20aa39bd629db93a1ec3cc1058
pasta-pasta <optic609@gmail.com>
2024-05-16 15:50:05 +0300

    code

---
+++ code_snippets/instant_death.lua
@@ -0,0 +1,17 @@
+
+function instantDeath(target)
+    
+    if target:hasEffect(target.EFF_INSTANT_DEATH_RESISTANCE) then
+        local resistance = target:getEffect(target.EFF_INSTANT_DEATH_RESISTANCE)
+        local randomValue = math.random(1, 100)
+        if randomValue <= resistance then
+            return
+        end
+    end
+
+    target.loaded = false
+
+    if target.onDeath then
+        target:onDeath()
+    end
+end
