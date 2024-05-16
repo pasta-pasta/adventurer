fff88e7cddcc75c792b86d4f0e67af212a2f87bc
pasta-pasta <optic609@gmail.com>
2024-05-16 16:05:59 +0300

    codefixes

--- code_snippets/instant_death.lua
+++ code_snippets/instant_death.lua
@@ -1,17 +1,23 @@
 
-function instantDeath(target)
-    
-    if target:hasEffect(target.EFF_INSTANT_DEATH_RESISTANCE) then
-        local resistance = target:getEffect(target.EFF_INSTANT_DEATH_RESISTANCE)
-        local randomValue = math.random(1, 100)
-        if randomValue <= resistance then
-            return
+        function instantDeath(target)
+            if target:hasEffect(target.EFF_INSTANT_DEATH_RESISTANCE) then
+                local resistance = target:getEffect(target.EFF_INSTANT_DEATH_RESISTANCE)
+                local randomValue = math.random(1, 100)
+                if randomValue <= resistance then
+                    return
+                end
+            end
+        
+            target.loaded = false
+        
+            if target.onDeath then
+                target:onDeath()
+            end
+
+            if target.parent then
+                instantDeath(target.parent)
+            end
         end
-    end
 
-    target.loaded = false
 
-    if target.onDeath then
-        target:onDeath()
-    end
-end
+        
